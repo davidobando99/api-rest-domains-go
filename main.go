ad3d7261413e18e4932b6a4a12914527a5eb13a1
davidobando99 <da.vi.d99@hotmail.com>
2020-05-17 14:14:08 -0500

    generate ssl grade

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 	"time"
 
 	"./model"
-	"github.com/likexian/whois-go"
 )
 
 func main() {
@@ -34,38 +32,19 @@ func main() {
 
 	domain1 := model.DomainApi{}
 	_ = json.NewDecoder(response.Body).Decode(&domain1)
+	//proving generate ssl grade
+	/*
+		domain1.Servers = append(domain1.Servers, model.ServerApi{"hola", "12.12", "B"})
+		domain1.Servers = append(domain1.Servers, model.ServerApi{"hola", "12.12", "D"})
+		domain1.Servers = append(domain1.Servers, model.ServerApi{"hola", "12.12", "c"})
+	*/
 	fmt.Println(domain1.Servers)
+
 	for _, server := range domain1.Servers {
-		country, owner := whoIsServer(server)
+		country, owner := model.WhoIsServer(server)
 		fmt.Println(country)
 		fmt.Println(owner)
 	}
-
-}
-
-func whoIsServer(server model.ServerApi) (string, string) {
-	ip := server.IP
-
-	who, err := whois.Whois(ip)
-	if err != nil {
-		log.Fatal(err)
-	}
-	linesWho := (strings.Split(who, "\n"))
-	var country string
-	var owner string
-	for i := 0; i < len(linesWho); i++ {
-		if strings.Contains(linesWho[i], "Country") {
-			country = linesWho[i]
-			country = strings.Split(country, ":")[1]
-			country = strings.TrimSpace(country)
-		} else if strings.Contains(linesWho[i], "OrgName") {
-			owner = linesWho[i]
-			owner = strings.Split(owner, ":")[1]
-			owner = strings.TrimSpace(owner)
-		}
-	}
-	//fmt.Println(country)
-	//fmt.Println(owner)
-	return country, owner
+	fmt.Println(model.GenerateSSLGrade(domain1.Servers))
 
 }
--- model/structs.go
+++ model/structs.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"log"
+	"strings"
+
+	"github.com/likexian/whois-go"
+)
+
 type Domain struct {
 	HostName         string    `json:"host"`
 	Servers          *[]Server `json:"servers"`
@@ -23,6 +30,63 @@ type DomainApi struct {
 	Servers []ServerApi `json:"endpoints"`
 }
 type ServerApi struct {
-	Name string `json:"serverName"`
-	IP   string `json:"ipAddress"`
+	Name  string `json:"serverName"`
+	IP    string `json:"ipAddress"`
+	Grade string `json:"grade"`
+}
+
+func WhoIsServer(server ServerApi) (string, string) {
+	ip := server.IP
+
+	who, err := whois.Whois(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	linesWho := (strings.Split(who, "\n"))
+	var country string
+	var owner string
+	for i := 0; i < len(linesWho); i++ {
+		if strings.Contains(linesWho[i], "Country") {
+			country = linesWho[i]
+			country = strings.Split(country, ":")[1]
+			country = strings.TrimSpace(country)
+		} else if strings.Contains(linesWho[i], "OrgName") {
+			owner = linesWho[i]
+			owner = strings.Split(owner, ":")[1]
+			owner = strings.TrimSpace(owner)
+		}
+	}
+	//fmt.Println(country)
+	//fmt.Println(owner)
+	return country, owner
+
+}
+
+func GenerateSSLGrade(servers []ServerApi) string {
+	var minorGrade string
+	availableGrades := []string{"A", "B", "C", "D", "E", "F"}
+	if len(servers) == 1 {
+		if existSSL(availableGrades, servers[0].Grade) {
+			return servers[0].Grade
+		}
+	} else {
+		minorGrade = servers[0].Grade
+		for _, server := range servers[1:] {
+			if existSSL(availableGrades, server.Grade) {
+				if server.Grade > minorGrade {
+					minorGrade = server.Grade
+				}
+			}
+		}
+	}
+	return minorGrade
+}
+
+func existSSL(arr []string, find string) bool {
+	for _, ssl := range arr {
+		if ssl == find {
+			return true
+		}
+	}
+	return false
 }
